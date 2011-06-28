4edeeb7a17dcc7205706467ac2702c340d179bb0
Don Selkirk <don@avelica.com>
2011-06-28 19:10:57 -0400

    big changes
    fixed class removal
    changed browser detection
    simplified ready list
    changed autoload to polyfill option
    added additional browser checks.
    removed packs
    added screen resolution classes

--- src/load.js
+++ src/load.js
@@ -1,7 +1,6 @@
-/* LOAD - 20110627-v1.4 */
+/* LOAD - 20110628-v1.4 */
 jQuery.noConflict();
 jQuery(function($){
-  var pack = ready.pack();
   var list = ready();
   var path = ready.path;
   
@@ -17,48 +16,62 @@ jQuery(function($){
         if(req.length==0) { if($.isFunction(call)) call($); return; }
         var item = req.pop();
         if(item){
-          if(pack[item]){
-            req = $.merge(pack[item],req); load();
-          }else{
-            var url = item;
-            if(url.substr(0,1)!='/') url = path + url;
-            $.getScript(url,load);
-          }
+          var url = item;
+          if(url.substr(0,1)!='/') url = path + url;
+          $.getScript(url,load);
         }
       }
       load();
     }
   }
   
+  var html = $('html');
+  
+  var debounce = function (func, threshold) {
+    var timeout;
+    return function () {
+      var obj = this,
+      args = arguments,
+      delayed = function () {
+        func.apply(obj, args);
+        timeout = null;
+      };
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(delayed, 50);
+    };
+  };
+  
+  var resize = function(){
+    html[0].className=html[0].className.replace(/ (w|lt)-\d+/g, '');
+    var w = $(this).width();
+    var sizes = [1920,1680,1440,1280,1024,768,640,480,320];
+    for(i in sizes) { i=sizes[i]; html.addClass( (i>=w?'lt':'gt')+'-'+i); }
+  }
+  
+  $(window).bind('resize', debounce(resize)).bind('orientationchange',resize);
+  
+  // execute preconfigured scripts
   $(list).each(function(){
-    if(this.req) window.ready(this.req, this.call);
-    else window.ready(this.call);
+    if(this.length>1) window.ready(this[0], this[1]);
+    else window.ready(this[0]);
   });
   
-  var autoloads = [{
-    sel: 'form.validate', req: 'validate.js', call: function(){ $(this).validate(); }
-  },{
-    sel: 'input.hint', req: 'hint.js'
-  },{
-    sel: 'div.flash', req: 'flash.js', call: function(){ $(this).flash(); }
-  }];
-  
-  // autoloads
-  $.each(autoloads,function(){
-    var item = this;
-    if(item.sel) {
-      var list = $(item.sel);
-      if(list.length>0) window.ready(item.req,function(){
-        if(item.call) list.each(item.call)
-      });
-    } else { if(item.test()) window.ready(item.req,item.call); }
-  });
+  // autoload polyfills
+  var polys = window.polyfil||{};
+  polys['placeholder'] = 'input[placeholder]';
+  for(var fill in polys) {
+    var test = polys[fill];
+    if(typeof(test)=='function'){
+      if(!test()) break;
+    }else{
+      if($(test).length==0) break;
+    }
+    window.ready(fill+'.js');
+  }
   
   // template based classes and scripts
   $('meta[name=layout]').each(function(){
-    var name = $(this).attr('content');
-    //$('html').addClass(name);
-    window.ready(name+'.js');
+    window.ready($(this).attr('content')+'.js');
   });
   
   //google
--- src/ready.js
+++ src/ready.js
@@ -1,41 +1,33 @@
-/* READY - 20110627-v1.1 */
-;(function(win){
-  var doc = win.document;
+/* READY - 20110628-v1.3 */
+;(function(win,doc){
   var html = doc.documentElement; /* html element */
-  html.className = html.className.replace(/\\bno-js\\b/, ''); /* remove the /no-js class */
+  html.className = html.className.replace(/\bno-js\b/, ''); /* remove the /no-js class */
   var addCls = function(n) { html.className += ' ' + n; } /* function for adding additional classes */
   
   /* useragent detection */
   var ua = win.navigator.userAgent.toLowerCase();
-  ua = /(webkit)[ \/]([\w.]+)/.exec( ua ) || /(opera)(?:.*version)?[ \/]([\w.]+)/.exec( ua ) || /(msie) ([\w.]+)/.exec( ua ) || !/compatible/.test( ua ) && /(mozilla)(?:.*? rv:([\w.]+))?/.exec( ua ) || [];
-  /* handling ie */
-  if (ua[1] == 'msie') {
-    ua = ['ie', doc.documentMode || ua[2]];
-    addCls('ie'+ua[2]); /* ie version classes. ex. ie6 */
-    if(ua[2]<=9) addCls('pie'); /* pie specific class for older ie */
-    var html5 = "abbr|article|audio|canvas|details|figcaption|figure|footer|header|hgroup|nav|output|section|summary|time|video".split("|"); /* allow html5 elements to be created */
-    for (var i = 0; i < html5.length; i++) doc.createElement(html5[i]);
+  var is = function(n,r){ if(ua.indexOf(n)>-1) return r||n; }
+  var br = is('ipad') || is('iphone') || is('blackberry') || is('webkit') || is('opera','o') || is('msie','ie') || ua.indexOf('compatible') < 0 && is('mozilla','moz') || '';
+  addCls(br);
+  if(br=='ie'){
+    var ver = doc.documentMode || /(msie) ([\w.]+)/.exec( ua )[2];
+    addCls('ie'+ver);
+    if(ver<=9) addCls('pie');
+    /* allow html5 elements to be created */
+    var html5 = 'article|audio|details|figure|footer|header|hgroup|nav|output|section|summary|time|video'.split('|');
+    for(i in html5) doc.createElement(html5[i]);
   }
-  addCls(ua[1]); /* browser specific class */
+
+  var w = window.innerWidth || html.clientWidth || 0;
+  var sizes = [1920,1680,1440,1280,1024,768,640,480,320];
+  for(i in sizes) { i=sizes[i]; addCls((i>=w?'lt':'gt')+'-'+i); }
   
   var list = [];
   var ready = function() {
-    var args = arguments;
-    if(args.length==0) return list;
-    var o = { call: args[0] };
-    if(args.length>1) o = { req: args[0], call: args[1] };
-    list[list.length] = o;
-    return win
+    if(arguments.length==0) return list;
+    list[list.length] = arguments;
   }
-
-  var pack = [];
-  ready.pack = function(){
-    var args = arguments;
-    if(args.length==0) return pack;
-    pack[args[0]] = args[1];
-    return win
-  }
-  
-  ready.path = '/scripts/';
-  win.ready = ready;
-})(window);
+  ready.path = '/scripts/'; /* preset script path */
+  win.ready = ready; /* expose ready function */
+  win.polyfill = []; /* register polyfill object */
+})(window,document);
